b2bf69faf7b9c9db890cfc75f387f87e2a1fecbe
missdays <dyeneffer13@hotmail.com>
2024-02-17 20:44:27 +0000

    add static images to project

--- garage/settings.py
+++ garage/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["http://127.0.0.1:8000/","127.0.0.1", ".herokuapp.com"]
 
