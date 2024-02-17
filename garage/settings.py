76db2e787c6feabd25a5c74ee34f9e24e55de694
missdays <dyeneffer13@hotmail.com>
2024-02-17 18:37:12 +0000

    readies code for deploy

--- garage/settings.py
+++ garage/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-r0)!omxjys-ojy22k_g=nz0b8ahww0mo=#mrdcbr_2-ya573&9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["http://127.0.0.1:8000/","127.0.0.1"]
+ALLOWED_HOSTS = ["http://127.0.0.1:8000/","127.0.0.1", ".herokuapp.com"]
 
 
 # Application definition
