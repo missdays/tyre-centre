ba206271ded6f201715feb114601151f5a6143fe
missdays <dyeneffer13@hotmail.com>
2024-02-17 21:02:34 +0000

    add allauth package

--- garage/urls.py
+++ garage/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from mainpage.views import my_mainpage
 
 
 urlpatterns = [
+    path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
     path('mainpage/', my_mainpage, name='mainpage'),
 ]
