a40c73a5d9370226182c3579ede505aa006fd7fc
missdays <dyeneffer13@hotmail.com>
2024-02-17 19:01:32 +0000

    configure database and manage environment variables

--- garage/settings.py
+++ garage/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r0)!omxjys-ojy22k_g=nz0b8ahww0mo=#mrdcbr_2-ya573&9'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
