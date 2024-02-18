0a384ed6980df5931585a36f92596b5fcd167539
missdays <dyeneffer13@hotmail.com>
2024-02-18 09:09:59 +0000

    add loginrequired tag to booking methods

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from django import forms
@@ -5,10 +6,13 @@ from .forms import BookingForm
 from .models import Booking
 
 # Create your views here.
+
+@login_required
 def bookings(request):
     user_bookings = Booking.objects.filter(user=request.user)
     return render(request, 'booking/booking.html', {'user_bookings': user_bookings})
 
+@login_required
 def create_booking(request):
     if request.method == 'POST':
         form = BookingForm(request.POST)
@@ -28,12 +32,14 @@ def create_booking(request):
     
     return render(request, 'booking/create_booking.html', {'form': form})
 
+@login_required
 def delete_booking(request, booking_id):
     booking = get_object_or_404(Booking, id=booking_id)
     booking.delete()
     messages.success(request, "Booking deleted successfully.")
     return redirect('booking')
 
+@login_required
 def edit_booking(request, booking_id):
     booking = get_object_or_404(Booking, id=booking_id)
     if request.method == 'POST':
