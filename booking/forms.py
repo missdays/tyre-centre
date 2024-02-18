6827be1b9d4145a2968e4f6b637120ba9513effb
missdays <dyeneffer13@hotmail.com>
2024-02-18 07:27:39 +0000

    set datetime field and logged in user

--- booking/forms.py
+++ booking/forms.py
@@ -5,3 +5,6 @@ class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
         fields = ['user', 'booking_date_and_time', 'status', 'observation']
+        widgets = {
+            'booking_date_and_time': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        }
--- booking/views.py
+++ booking/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.contrib import messages
+from django import forms
 from .forms import BookingForm
 
 # Create your views here.
@@ -8,13 +9,19 @@ def create_booking(request):
     if request.method == 'POST':
         form = BookingForm(request.POST)
         if form.is_valid():
-            form.save()
+            booking = form.save(commit=False)
+            booking.user = request.user
+            booking.save()
             messages.add_message(
                 request, messages.SUCCESS,
                 'Booking request received! Await confirmation'
             )
-            form = BookingForm()
+            form = BookingForm(initial={'user': request.user})
+            form.fields['user'].widget = forms.HiddenInput()
+            form.fields['status'].widget = forms.HiddenInput()
     else:
-        form = BookingForm()
+        form = BookingForm(initial={'user': request.user})
+        form.fields['user'].widget = forms.HiddenInput()
+        form.fields['status'].widget = forms.HiddenInput()
     
     return render(request, 'booking/booking.html', {'form': form})
